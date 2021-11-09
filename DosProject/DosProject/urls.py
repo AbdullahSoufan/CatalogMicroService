17449a740727f50a2863586228ba63037db77b6a
AbdullahSoufan <Abdullah.Soufan@hotmail.com>
2021-11-09 20:33:24 +0200

    Import_bug_fixed

--- DosProject/DosProject/urls.py
+++ DosProject/DosProject/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import include, path
-from DosProject.CatalogApp import views
+from CatalogApp import views
 
 urlpatterns = [
     path('search/<str:topic_name>/', views.bookList.as_view()),
